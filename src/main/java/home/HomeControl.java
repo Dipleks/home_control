7879378f03a76e73f2eab5472972d60f6dc01843
dipleks <gladiator9000@inbox.ru>
2019-02-24 22:03:11 +0300

    test

---
+++ src/main/java/home/HomeControl.java
@@ -0,0 +1,15 @@
+package home;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class HomeControl extends Application {
+
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+}
